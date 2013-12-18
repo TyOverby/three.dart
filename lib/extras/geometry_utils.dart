d5a10d34e707cce2233d5029cbb4d7367ccf859b
TyOverby <ty@pre-alpha.com>
2013-12-18 01:06:39 -0800

    Implemented GeometryUtils.merge

--- lib/extras/geometry_utils.dart
+++ lib/extras/geometry_utils.dart
@@ -4,6 +4,119 @@ import "package:three/three.dart";
 import 'package:vector_math/vector_math.dart';
 
 // TODO(nelsonsilva) - Add remaining functions
+
+void merge(Geometry geometry1, Object3D object2 /* mesh | geometry */, [int materialIndexOffset = 0]) {
+
+  Matrix4 matrix, normalMatrix;
+  int vertexOffset = geometry1.vertices.length;
+  int uvPosition = geometry1.faceVertexUvs[ 0 ].length;
+  Geometry geometry2 = ( object2 is Mesh ) ? object2.geometry : object2;
+  
+  List<Vector3> vertices1 = geometry1.vertices;
+  List<Vector3> vertices2 = geometry2.vertices;
+  
+  List<Face> faces1 = geometry1.faces;
+  List<Face> faces2 = geometry2.faces;
+  
+  List uvs1 = geometry1.faceVertexUvs[ 0 ];
+  List uvs2 = geometry2.faceVertexUvs[ 0 ];
+
+
+  if ( object2 is Mesh ) {
+
+    object2.matrixAutoUpdate && object2.updateMatrix();
+
+    matrix = object2.matrix;
+
+    var tmp = new Matrix4.identity();
+    tmp.invert();
+    normalMatrix = tmp.transposed();
+  }
+
+  // vertices
+
+  for ( var i = 0, il = vertices2.length; i < il; i ++ ) {
+    
+    var vertex = vertices2[ i ];
+    
+    var vertexCopy = vertex.clone();
+    
+    if ( matrix != null ) vertexCopy.applyProjection( matrix );
+    
+    vertices1.add( vertexCopy );
+  }
+
+  // faces
+
+  for ( var i = 0, il = faces2.length; i < il; i ++ ) {
+    
+    var face = faces2[ i ], faceCopy, normal, color,
+        faceVertexNormals = face.vertexNormals,
+        faceVertexColors = face.vertexColors;
+    
+    faceCopy = new Face3( face.a + vertexOffset, face.b + vertexOffset, face.c + vertexOffset );
+    
+    faceCopy.normal.setFrom( face.normal );
+    
+    if ( normalMatrix != null ) {
+      
+      faceCopy.normal.applyProjection( normalMatrix ).normalize();
+      
+    }
+
+    for ( var j = 0, jl = faceVertexNormals.length; j < jl; j ++ ) {
+      
+      normal = faceVertexNormals[ j ].clone();
+      
+      if ( normalMatrix != null ) {
+        
+        normal.applyMatrix3( normalMatrix ).normalize();
+        
+      }
+      
+      faceCopy.vertexNormals.add( normal );
+      
+    }
+
+    faceCopy.color.copy( face.color );
+
+    for ( var j = 0, jl = faceVertexColors.length; j < jl; j ++ ) {
+      
+      color = faceVertexColors[ j ];
+      faceCopy.vertexColors.add( color.clone() );
+      
+    }
+
+    faceCopy.materialIndex = face.materialIndex + materialIndexOffset;
+    
+    faceCopy.centroid.setFrom( face.centroid );
+
+    if ( matrix != null ) {
+      
+      faceCopy.centroid.applyProjection( matrix );
+      
+    }
+    faces1.add( faceCopy );
+
+  }
+
+  // uvs
+
+  for ( var i = 0, il = uvs2.length; i < il; i ++ ) {
+
+    var uv = uvs2[ i ], uvCopy = [];
+
+    for ( var j = 0, jl = uv.length; j < jl; j ++ ) {
+
+      uvCopy.add( new Vector2( uv[ j ].x, uv[ j ].y ) );
+
+    }
+
+    uvs1.add( uvCopy );
+  }
+
+}
+
 clone( Geometry geometry ) {
 
     var cloneGeo = new Geometry();
